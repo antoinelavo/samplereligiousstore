ed4c5e0b496be5378105533faf65822e8f3391b8
antoinelavo <eugenepark912@gmail.com>
2025-07-04 01:44:25 +0900

    fixed images

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disables Vercel's image optimization
+  },
 };
 
 export default nextConfig;
